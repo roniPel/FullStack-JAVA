928c29b0dd317d0bb58c99686a72c7732deb92cd
RoniPel <roni.rose@gmail.com>
2024-02-19 15:45:48 +0200

    Create spring coupon system

---
+++ MyProjects/CouponSystem2/src/main/java/JavaProject/CouponSystem2/CouponSystem2SpringApplication.java
@@ -0,0 +1,13 @@
+package JavaProject.CouponSystem2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CouponSystem2SpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CouponSystem2SpringApplication.class, args);
+	}
+
+}
---
+++ MyProjects/CouponSystem2/src/test/java/JavaProject/CouponSystem2/CouponSystem2SpringApplicationTests.java
@@ -0,0 +1,13 @@
+package JavaProject.CouponSystem2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CouponSystem2SpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
