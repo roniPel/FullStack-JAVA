eba936f576f6f2b9a8ab530598981655d9a815a5
RoniPel <roni.rose@gmail.com>
2024-04-15 14:28:59 +0300

    sync

--- 14_Web/10.04.2024/ClassExercise5_100424/js/domManipulation.js
+++ 14_Web/10.04.2024/ClassExercise5_100424/js/domManipulation.js
@@ -4,7 +4,8 @@ function checkLogin() {
     var password = document.getElementById("password").value;
     // Check login details
     if(userName == 'ADMIN' && password == 'ADMIN@ADMIN.com') {
-        document.getElementById("displayLoginResult").innerText = `Hello, My Master ${userName}`;
+        document.getElementById("displayLoginResult").innerText = "Hello, My Master ${userName}";
+        //document.getElementById("displayLoginResult").innerHTML = "<b>Hello, My Master!</b>";
     }
     else {
         document.getElementById("displayLoginResult").innerText = 'Why, who are you???';
