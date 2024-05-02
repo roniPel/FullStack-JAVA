2de2e50a20b7b30613ac5d606c3e9adbcf707ba9
RoniPel <roni.rose@gmail.com>
2024-05-02 16:30:07 +0300

    Sync

--- MyProjects/CouponSystem3_Spring/src/main/java/JavaProject/CouponSystem2_Spring/Repositories/UsersRepo.java
+++
@@ -1,8 +0,0 @@
-package JavaProject.CouponSystem2_Spring.Repositories;
-
-import JavaProject.CouponSystem2_Spring.Beans.Credentials;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UsersRepo extends JpaRepository<Credentials,Integer> {
-    Credentials findByUserNameAndUserPass(String userName, String userPass);
-}
