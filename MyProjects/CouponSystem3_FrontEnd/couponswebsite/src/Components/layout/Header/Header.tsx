afe98e6d1a5e0f3a0352070e509d557b2a3015dc
RoniPel <roni.rose@gmail.com>
2024-05-21 15:55:21 +0300

    Adding Redux - Auth Reducer + Store

--- MyProjects/CouponSystem3_FrontEnd/couponswebsite/src/Components/layout/Header/Header.tsx
+++ MyProjects/CouponSystem3_FrontEnd/couponswebsite/src/Components/layout/Header/Header.tsx
@@ -2,10 +2,22 @@ import "./Header.css";
 import logo from "../../../Assets/Logo.png";
 import { useNavigate } from "react-router-dom";
 import { Button, ButtonGroup, Typography } from "@mui/material";
+import { useState } from "react";
+import { couponStore } from "../../../Redux/store";
+import { logoutAction } from "../../../Redux/authReducer";
 
 //TODO: Change LoginHeadline to: Hello, {user}}!
 export function Header(): JSX.Element {
+    const [isLogged, setLogged] = useState(false);
+    const [userName, setName] = useState("");
+
+    couponStore.subscribe(() => {
+        setName(couponStore.getState().auth.name);
+        setLogged(couponStore.getState().auth.isLogged);
+    })
+
     const navigate = useNavigate();
+    
     return (
         <div className="Header">
             <div>
@@ -15,10 +27,18 @@ export function Header(): JSX.Element {
                 <Typography variant="h2" className="MainHeadLine">Coupons System</Typography>
             </div>
             <div className="login">
-            <Typography variant="h6" className="LoginHeadline">Hello, XXX!</Typography>           
+                Hello {userName} <br />
                 <ButtonGroup variant="contained" fullWidth>
-                        <Button type="submit" color="primary" onClick={()=>{navigate("/login")}}>login</Button>
-                        <Button color="success" onClick={() => { navigate("/register") }}>register</Button>
+                    <Button type="submit" color={isLogged ? "error" : "primary"}
+                        onClick={() => {
+                            if (isLogged) {
+                                couponStore.dispatch(logoutAction());
+                                navigate("/");
+                            } else {
+                                navigate("/login");
+                            }
+                        }}>{isLogged ? "Logout" : "Login"}</Button>
+                    {!isLogged && <Button color="success" onClick={() => { navigate("/register") }}>register</Button>}
                 </ButtonGroup>
             </div>
         </div>
--- MyProjects/CouponSystem3_FrontEnd/couponswebsite/src/Components/layout/Menu/Menu.tsx
+++ MyProjects/CouponSystem3_FrontEnd/couponswebsite/src/Components/layout/Menu/Menu.tsx
@@ -1,8 +1,48 @@
 import { NavLink } from "react-router-dom";
 import "./Menu.css";
 import { Typography } from "@mui/material";
+import { useState } from "react";
+import { couponStore } from "../../../Redux/store";
 
 export function Menu(): JSX.Element {
+
+    const [isAdmin,setAdmin] = useState(false);
+
+    couponStore.subscribe(()=>{
+        setAdmin(couponStore.getState().auth.userType==="ADMIN");
+    });
+    
+    const simpleMenu = ()=>{
+        return (
+            <>
+                <br/><Typography variant="h4" className="HeadLine">Menu</Typography><br/>
+                <hr/>
+                <NavLink to="/">Home</NavLink><br/>
+                <NavLink to="/aboutus">About Us</NavLink><br/>
+                <hr/>
+            </>
+        )
+    }
+
+    const adminMenu = ()=>{
+        return (
+            <>
+                <NavLink to="/">User List</NavLink><br/>
+                <NavLink to="/">Find User</NavLink><br/>
+                <NavLink to="/">Delete User</NavLink><br/>
+                <hr/>
+            </>
+        )
+    }
+    return (
+        <div className="Menu">
+			{simpleMenu()}
+            <hr/>
+            {isAdmin && adminMenu()}
+        </div>
+    );
+
+
     return (
         <div className="Menu">
             <br/><Typography variant="h4" className="HeadLine">Menu</Typography><br/><hr/>
