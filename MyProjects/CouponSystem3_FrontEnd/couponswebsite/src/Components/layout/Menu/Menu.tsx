8a37fdcff407f66ecae7b52da7f79986ad007e68
RoniPel <roni.rose@gmail.com>
2024-05-22 10:14:12 +0300

    Menus for Admin, Company, Customer

--- MyProjects/CouponSystem3_FrontEnd/couponswebsite/src/Components/layout/Menu/Menu.tsx
+++ MyProjects/CouponSystem3_FrontEnd/couponswebsite/src/Components/layout/Menu/Menu.tsx
@@ -7,6 +7,8 @@ import { couponStore } from "../../../Redux/store";
 export function Menu(): JSX.Element {
 
     const [isAdmin,setAdmin] = useState(false);
+    const [isCompany,setCompany] = useState(false);
+    const [isCustomer,setCustomer] = useState(false);
 
     couponStore.subscribe(()=>{
         setAdmin(couponStore.getState().auth.userType==="ADMIN");
@@ -23,16 +25,59 @@ export function Menu(): JSX.Element {
         )
     }
 
+    //Todo - Change the 'to' in NavLinks for 'getOneCoupon' (so it won't be the same as admin) + update 'MainRoutes'
     const adminMenu = ()=>{
         return (
             <>
-                <NavLink to="/">User List</NavLink><br/>
-                <NavLink to="/">Find User</NavLink><br/>
-                <NavLink to="/">Delete User</NavLink><br/>
+                <NavLink to="/userId">Admin Home</NavLink><br/>
+                <NavLink to="/addCompany">Add Company</NavLink><br/>
+                <NavLink to="/deleteCompany">Delete Company</NavLink><br/>
+                <NavLink to="/deleteCustomer">Delete Customer</NavLink><br/>
+                <NavLink to="/getAllCompanies">View All Companies</NavLink><br/>
+                <NavLink to="/getAllCustomers">View All Customers</NavLink><br/>
+                <NavLink to="/getOneCompany">View One Company</NavLink><br/>
+                <NavLink to="/getOneCustomer">View One Customer</NavLink><br/>
+                <NavLink to="/updateCompany">Update Company</NavLink><br/>
+                <NavLink to="/updateCustomer">Update Customer</NavLink><br/>
                 <hr/>
             </>
         )
     }
+
+    const companyMenu = ()=>{
+        return (
+            <>
+                <NavLink to="/userId">Company Home</NavLink><br/>
+                <NavLink to="/">Add Coupon</NavLink><br/>
+                <NavLink to="/">Delete Coupon</NavLink><br/>
+                <NavLink to="/">View My Coupons</NavLink><br/>
+                <NavLink to="/">View My Coupons by Category</NavLink><br/>
+                <NavLink to="/">View My Coupons by Max Price</NavLink><br/>
+                <NavLink to="/">View My Details</NavLink><br/>
+                <NavLink to="/">View One Coupon</NavLink><br/>
+                <NavLink to="/">Update Coupon</NavLink><br/>
+                <hr/>
+            </>
+        )
+    }
+
+    const customerMenu = ()=>{
+        return (
+            <>
+                <NavLink to="/userId">Customer Home</NavLink><br/>
+                <NavLink to="/">View All Coupons</NavLink><br/>
+                <NavLink to="/">View All Customers</NavLink><br/>
+                <NavLink to="/">View My Coupons</NavLink><br/>
+                <NavLink to="/">View My Coupons by Category</NavLink><br/>
+                <NavLink to="/">View My Coupons by Max Price</NavLink><br/>
+                <NavLink to="/">View My Details</NavLink><br/>
+                <NavLink to="/">View One Coupon</NavLink><br/>
+                <NavLink to="/">Purchase Customer</NavLink><br/>
+                <hr/>
+            </>
+        )
+    }
+
     return (
         <div className="Menu">
 			{simpleMenu()}
