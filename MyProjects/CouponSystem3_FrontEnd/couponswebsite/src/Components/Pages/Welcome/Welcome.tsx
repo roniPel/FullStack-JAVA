1ccda923d7523f30896aa63d486ac8b7611da682
RoniPel <roni.rose@gmail.com>
2024-05-30 16:14:17 +0300

    Update 'UseEffect' in 'Welcome' screen

--- MyProjects/CouponSystem3_FrontEnd/couponswebsite/src/Components/Pages/Welcome/Welcome.tsx
+++ MyProjects/CouponSystem3_FrontEnd/couponswebsite/src/Components/Pages/Welcome/Welcome.tsx
@@ -5,16 +5,29 @@ import { useEffect, useState } from "react";
 import { Coupon } from "../../../Models/Coupon";
 import axios from "axios";
 import { SingleCoupon } from "../SingleCoupon/SingleCoupon";
+import { checkData } from "../../../Utilities/checkData";
+import { couponStore } from "../../../Redux/store";
 
 export function Welcome(): JSX.Element {
     
     //react hooks => useState , useEffect
     const [couponList, setList] = useState<Coupon[]>([]);
+    const navigate = useNavigate();
 
     //get song list from backend
     useEffect(()=>{
         let recivedList:Coupon[] = [];
-        axios.get("http://localhost:8080/Guest/GetAllCoupons").then(result=>{
+
+         //check if we have any coupons on our list, 0 length indicates that we don't have any song
+         if (couponStore.getState().auth.token.length<10){
+            navigate("/login");
+        }
+        checkData();
+
+        //if(couponStore.getState().coupons.allCoupons.length == 0)
+        if (true) {
+            axios.get("http://localhost:8080/Guest/GetAllCoupons")
+            .then(result=>{
             for (let index=0;index<result.data.length;index++){
                 recivedList.push(new Coupon(
                     result.data[index].id,
@@ -29,14 +42,22 @@ export function Welcome(): JSX.Element {
                     result.data[index].image
                 ));
             };
-            setList(recivedList);
-        });
-    },[])
+            setList(recivedList);                   
+                // couponStore.dispatch(getAllCouponsAction(recivedList));
+                // setList(couponStore.getState().coupons.allCoupons);
+                })
+                .catch(err=>{
+                    navigate("/login")
+                });
+        } else {
+            //setList(couponStore.getState().coupons.allCouponss);
+        }
+        
+    },[]);
 
     return (
         <div className="Welcome">
             <br/><Typography variant="h4" className="HeadLine">Available Coupons</Typography><hr/><br/>
-            {/* {songList.map(item=><div className="Box">{item.id}<br/>{item.name}<br/>{item.desc}<br/><img src={item.image} width={100}/></div>)} */}
             {couponList.map(item=><SingleCoupon key={item.id} coupon={item}/>)}
         </div>
     );
