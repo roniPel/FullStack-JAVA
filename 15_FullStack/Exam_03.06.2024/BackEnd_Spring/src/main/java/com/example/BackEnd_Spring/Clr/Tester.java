3eefcb90ca476cef63c61a06fb18d0c264e9cf42
RoniPel <roni.rose@gmail.com>
2024-06-03 10:23:41 +0300

    Preparation for exam - backend

--- 15_FullStack/Exam_03.06.2024/BackEnd_Spring/src/main/java/com/example/BackEnd_Spring/Clr/Tester.java
+++ 15_FullStack/Exam_03.06.2024/BackEnd_Spring/src/main/java/com/example/BackEnd_Spring/Clr/Tester.java
@@ -1,3 +1,17 @@
-package com.example.BackEnd_Spring.Clr;public class Tester {
-    
+package com.example.BackEnd_Spring.Clr;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.core.annotation.Order;
+import org.springframework.stereotype.Component;
+
+@Component
+@Order(1)
+@RequiredArgsConstructor
+public class Tester implements CommandLineRunner {
+
+    @Override
+    public void run(String... args) throws Exception {
+
+    }
 }
