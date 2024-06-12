38d40d9504b22a757c80209a7f03a1d7cbd262ce
RoniPel <roni.rose@gmail.com>
2024-06-12 23:47:25 +0300

    Add useEffect (viewing permissions) for all admin pages

--- MyProjects/CouponSystem3_FrontEnd/couponswebsite/src/Components/Pages/Administrator/AddCompany/AddCompany.tsx
+++ MyProjects/CouponSystem3_FrontEnd/couponswebsite/src/Components/Pages/Administrator/AddCompany/AddCompany.tsx
@@ -1,6 +1,20 @@
+import { useNavigate } from "react-router-dom";
 import "./AddCompany.css";
+import { useEffect } from "react";
+import { ClientType } from "../../../../Models/ClientType";
+import { couponStore } from "../../../../Redux/store";
+import notify from "../../../../Utilities/notify";
 
 export function AddCompany(): JSX.Element {
+    const navigate = useNavigate();
+
+    useEffect(()=>{
+        // Check if user has viewing permissions
+        if (couponStore.getState().auth.clientType!==ClientType.Administrator){
+            navigate("/login");
+            notify.error("You are not allowed!!!");
+        }
+    },[]);
     return (
         <div className="AddCompany">
 			
--- MyProjects/CouponSystem3_FrontEnd/couponswebsite/src/Components/Pages/Administrator/AdminHome/AdminHome.tsx
+++ MyProjects/CouponSystem3_FrontEnd/couponswebsite/src/Components/Pages/Administrator/AdminHome/AdminHome.tsx
@@ -1,6 +1,21 @@
+import { useNavigate } from "react-router-dom";
 import "./AdminHome.css";
+import { useEffect } from "react";
+import { ClientType } from "../../../../Models/ClientType";
+import { couponStore } from "../../../../Redux/store";
+import notify from "../../../../Utilities/notify";
 
 export function AdminHome(): JSX.Element {
+    const navigate = useNavigate();
+
+    useEffect(()=>{
+        // Check if user has viewing permissions
+        if (couponStore.getState().auth.clientType!==ClientType.Administrator){
+            navigate("/login");
+            notify.error("You are not allowed!!!");
+        }
+    },[]);
+
     return (
         <div className="AdminHome">
 			<h1>Admin Home</h1>
