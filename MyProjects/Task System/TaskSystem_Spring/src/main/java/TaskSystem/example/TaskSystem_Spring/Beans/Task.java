4376231246129e270be8378ac5959c081fa2ef25
RoniPel <roni.rose@gmail.com>
2024-11-25 11:53:25 +0200

    Task System Beans

---
+++ MyProjects/Task System/TaskSystem_Spring/src/main/java/TaskSystem/example/TaskSystem_Spring/Beans/Task.java
@@ -0,0 +1,46 @@
+package TaskSystem.example.TaskSystem_Spring.Beans;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.hibernate.validator.constraints.Length;
+
+import java.time.LocalDate;
+
+@Entity
+@Table(name = "tasks")
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class Task {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Integer id;
+    // with a title, description, due date, priority level, and project category.
+    @Column(name = "title",
+            length = 50,
+            unique = true)
+    @Length(max = 50)
+    private String title;
+
+    @Column(name = "description",
+            length = 70)
+    @Length(max = 70)
+    private String description;
+    @Column(name = "due_date")
+    private LocalDate due_date;
+
+    @Enumerated(EnumType.ORDINAL)
+    @Column(name = "priority_id",
+            nullable = false)
+    private Integer priority;
+
+    @Column(name = "project_id",
+            nullable = false)
+    private Project project;
+
+}
